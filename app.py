4d3b2a40273c209559c4701948cc7f7bf39319a8
Douglas Saturnino <douglasaturnino@gmail.com>
2025-10-18 18:35:38 -0300

    Substitui mensagem fixa por resposta da IA no webhook do WhatsApp

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
-from loguru import logger as logging
 
+from chains import get_conversational_rag_chain
 from evolution_api import send_whatsapp_message
 
 app = FastAPI()
+
+conversational_rag_chain = get_conversational_rag_chain()
 
 
 @app.post("/webhook")
@@ -14,9 +16,14 @@ async def webhook(request: Request):
     message = data.get("data").get("message").get("conversation")
 
     if chat_id and message and from_me and "@g.us" not in chat_id:
+        ai_response = conversational_rag_chain.invoke(
+            input={"input": message},
+            config={"configurable": {"session_id": chat_id}},
+        )["answer"]
+
         send_whatsapp_message(
             number=chat_id,
-            text="Oi, fala ai mano",
+            text=ai_response,
         )
 
     return {"status": "ok"}
